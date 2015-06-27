ba24581bdf5214913d63145abbeb9b1b71302eda
Jelle Herold <jelle@defekt.nl>
2015-06-27 14:55:07 +0200

    split queries from things working on the structure of the graph

--- neo4j.js
+++ neo4j.js
@@ -13,28 +13,45 @@ var log = require('./log')
 var db = new Neo4J.GraphDatabase(u.format('http://%s:%s@%s:%s', c.username, c.password, c.host, c.port));
 var qs = YAML.parse(fs.readFileSync('neo4j.yaml', {encoding: 'utf8'}));
 
-var neo = R.curry(
-	function(kind, action, params){
-		var opts = {
-			params: params,
-			query: qs[kind][action].cypher
-		};
+// run cypher query and return promise
+var cypher = function(opts)
+{
+	var d = Q.defer();
+	db.cypher(opts, function(err,resp){
+		if(err) d.reject(err);
+		else d.resolve(resp);
+	});
+	return d.promise;
+}
+
+// definitions
+exports.queries = qs.queries
+exports.structures = qs.structures
+
+// promise makers
+exports.query = function(query_name, params)
+{
+	// lookup query
+	var q = qs.queries[query_name].cypher;
+
+	// report about it
+	log.QUERY(query_name, q, params);
+
+	// return promise
+	return cypher({params: params, query: q});
+}
+exports.structure = function(structure, action, params)
+{
+	// lookup query
+	var q = qs.structures[structure][action].cypher;
 	
-		var d = Q.defer();
-		log.QUERY(action, opts.query, params)
-		db.cypher(opts, function(err,resp){
-			if(err) d.reject(err);
-			else d.resolve(resp);
-		});
-		return d.promise;
-	}
-);
+	// report about it
+	log.STRUCTURE(structure, action, q, params);	
+	
+	// return promise
+	return cypher({params: params, query: q});
+}
 
-exports.queries = qs
-
-exports.query = neo('queries')
-exports.node = neo('node')
-exports.edge = neo('edge')
-
-// neo('node','add', {type: 'pit', id: 'id-1'}, {});
-// neo('edge','add', {type: 'same-as', s: 'id-1', t: 'id-1'}, {});
+// neo.structure('node','add', {type: 'pit', id: 'id-1'});
+// neo.structure('edge','add', {type: 'same-as', s: 'id-1', t: 'id-1'});
+// neo.query('klont', {id: 'id-1'});
