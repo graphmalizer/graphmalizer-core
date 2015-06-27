49bc68265a9fe025f15f772bc4f8597837840f82
Jelle Herold <jelle@defekt.nl>
2015-06-27 14:53:30 +0200

    generelize by structure, not necessary node or edge

--- type.js
+++ type.js
@@ -1,23 +1,45 @@
+var R = require('ramda');
 var u = require('util');
+
 var Cached = require('./cache');
 
+// we are screaming here, because it is really important
+var STRUCTURES = require('./neo4j').structures;
 var TYPES = require('./config').types;
+var TYPE_SPECIFIC = require('./typeSpecific');
 
-var Type = function (s){
+var Type = function (s)
+{
+	if(!s)
+		throw new Error("You must specify a type");
+
 	if(s && !TYPES[s])
-		throw new Error(u.format('Unkown type "%s", must be one of %s', type.name, Type.TYPES))
+		throw new Error(u.format('Unkown type "%s", must be one of %s',
+			s, Object.keys(TYPES).join(', ')));
 
-	this.name = s || '*';
+	// store the name
+	this.name = s;
 	
-	this.isNode = (TYPES[s] && TYPES[s].node && true) || false
-	this.isEdge = (TYPES[s] && TYPES[s].edge && true) || false
-	this.opts = {}
-	
-	if(this.isNode)
-		this.opts = TYPES[s].node;
+	// TODO move to config?
+	this.opts = TYPE_SPECIFIC[s];
 
-	if(this.isEdge)
-		this.opts = TYPES[s].edge;
+	// what kind of structure is it: node, arc, ..
+	var k = Object.keys(TYPES[s])[0]
+
+	if(!STRUCTURES[k])
+		throw new Error(u.format('Unknown structure "%s", must be one of %s',
+			k, Object.keys(STRUCTURES).join(', ')));
+
+	// store it
+	this.structure = k
+}
+
+Type.prototype.identifier = function(input) {
+	console.log("ADSF", this.opts);
+	if(this.opts.identifier)
+		return this.opts.identifier(input);
+
+	return input.id;
 }
 
 module.exports = Cached(Type);
