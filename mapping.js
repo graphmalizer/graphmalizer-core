66db9766f18093805976df5a31985b66926fa421
Jelle Herold <jelle@defekt.nl>
2015-06-27 03:20:27 +0200

    more lessenifying

--- mapping.js
+++ mapping.js
@@ -4,7 +4,6 @@ var Dataset = require('./dataset');
 var Type = require('./type');
 var log = require('./log');
 
-
 // create hash from a bunch of things
 var crypto = require('crypto');
 
@@ -16,76 +15,45 @@ var hashOf = function(things){
 	return shasum.digest('base64');
 }
 
+module.exports = function(dname, tname, id, source, target, doc)
+{	
+	var dataset = Dataset.cget(dname);
+	var type = Type.cget(tname);
 
-/*	Ensure string arguments are converted into normal form
+	log.DATASET(dataset);
+	log.TYPE(type);
 
-	dataset ~> Dataset
-	type ~> Type
-*/
-var normalize = function(f) {
-	return function(d, ty, id, source, target, doc)
+	// nodes must have an id!
+	if ((type.isNode) && (!id))
+			throw new Error(u.format('Must specify id when creating a node!'));
+
+	// we are dealing with an edge
+	if(type.isEdge)
 	{
-		var dataset = Dataset.cget(d);
-		var type = Type.cget(ty);
+		if(!source)
+			throw new Error(u.format("Missing source"));
 
-		log.DATASET(dataset);
-		log.TYPE(type);
+		if(!target)
+			throw new Error(u.format("Missing target"));
 
-		// nodes must have an id!
-		if(type.isNode){
+		// if no id is specified, build one
+		var eid = id || hashOf([
+			source.replace('/', '.'),
+			type.name,
+			target.replace('/', '.')
+		]);
+	}
 
-			if(!id)
-				throw new Error(u.format('Must specify id when creating a node!'));
-		
-			return f(dataset, type, id, undefined, undefined, doc);
-		}
-
-		// we are dealing with an edge
-		if(type.isEdge){
-
-			if(!source)
-				throw new Error(u.format("Missing source"));
-	
-			if(!target)
-				throw new Error(u.format("Missing target"));
-
-			// if no id is specified, build one
-			var eid = id || hashOf([
-					source.replace('/', '.'),
-					type.name,
-					target.replace('/', '.')
-				]);
-
-			return f(dataset, type, eid, source, target, doc);
-		}
-	} 
-}
-
-module.exports = normalize(
-	function(dataset, type, id, source_id, target_id, doc) {
-
-		var base = {
-			params: {
-				labels: [
-					'T_' + type.name, // double function: ES type, Node Type
-					'I_' + dataset.name, // subdataset owning this element
-					'__' // meaning, this is a managed node
-				],
-				id: id,
-				doc: doc
-			},
-			isNode: true,
-			isEdge: false
-		};
-
-		// add edge info
-		if(type.isEdge) {
-			base.isNode = false;
-			base.isEdge = true;
-			base.params.source = source_id;
-			base.params.target = target_id;
-		}
-		
-		return base;
+	return {
+		params: {
+			dataset: dname,
+			type: tname,
+			id: id || eid,
+			source: source,
+			target: target,
+			doc: doc
+		},
+		isNode: type.isNode,
+		isEdge: type.isEdge
 	}	
-);
+}
