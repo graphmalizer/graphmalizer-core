01720f90277f22f45b7f825e3934b9c10430af7f
Jelle Herold <jelle@defekt.nl>
2015-06-27 02:43:41 +0200

    remove old stuff

--- resources.js
+++ resources.js
@@ -3,12 +3,17 @@ var R = require('ramda');
 
 var log = require('./log');
 var mapping = require('./mapping');
-var connection = require('./backends')('http://neo4j:waag@localhost:7474');
+var neo = require('./neo4j');
 
 // operate on backend
 perform = function(operation, mapping) {
 	log.NEO(operation, mapping)
-	return connection[operation](mapping)
+
+	if(mapping.isNode)
+		return neo.node(operation, mapping.params);
+
+	if(mapping.isEdge)
+		return neo.edge(operation, mapping.params);
 };
 
 function resource(op){
@@ -31,37 +36,17 @@ function resource(op){
 	}
 }
 
-
-var queries = R.mapObj(
-	
-	// set key value to .metadata.description.trim()
-	R.compose(
-		R.trim(),
-		function noEmptyString(s){return s || '-'},
-		R.prop('description'),
-		R.prop('metadata')
-	),
-	
-	// load queries
-	require('./queries')
-);
-
 module.exports = {
 	POST: resource('add'),
 	PUT: resource('update'),
 	DELETE: resource('remove'),
 	cypher: function(conn) {
 		var args = conn.params;
-		return Q.fcall(function(){
-			return connection.cypher({
-				params: args,
-				query_name: args.query_name
-			});
-		});
+		return neo.query(args.query_name, args);
 	},
 	list_queries: function(coño) {
 		return Q.fcall(function(){
-			return queries
+			return neo.queries
 		});
 	}
 }
