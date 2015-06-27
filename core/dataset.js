eedd482d936e9c4dbd7fc082e396c195a997c285
Jelle Herold <jelle@defekt.nl>
2015-06-27 18:48:27 +0200

    dead code

--- core/dataset.js
+++ core/dataset.js
@@ -1,10 +1,8 @@
 var R = require('ramda');
-var Cached = require('./cache');
+var Cached = require('../utils/cache');
 
 var Dataset = function (s){
 	this.name = s || '*';
-	this.path = this.name.split('-');
-	this.root = R.head(this.path);
 };
 
 module.exports = Cached(Dataset);
