7bf32261565f44ffc43907e242086b54854d16df
Jelle Herold <jelle@defekt.nl>
2015-06-30 16:08:13 +0200

    batch neo4j

--- utils/neo4j.js
+++ utils/neo4j.js
@@ -1,23 +1,44 @@
 var u = require('util')
 var fs = require('fs')
-
 var Q = require('kew')
 
-var Neo4J = require('neo4j')
-
 var c = require('./config').Neo4J
-// var log = require('./log')
 
-var db = new Neo4J.GraphDatabase(u.format('http://%s:%s@%s:%s',c.username, c.password, c.host, c.port));
+var N = 100; // batch size
+
+var batchCommit = require('../utils/neo4batch');
+
+var qs = [];
+
+var trivial_promise = Q.fcall(function(){
+	return true;
+});
 
 // run cypher query and return promise
 var cypher = function(opts)
 {
+	// store the query
+	qs.push(opts);
+
+	// not at batch size yet, do nothing
+	if(qs.length < N)
+		return trivial_promise;
+
+	// ok, N-th query added, let's send whole batch
 	var d = Q.defer();
-	db.cypher(opts, function(err,resp){
-		if(err) d.reject(err);
-		else d.resolve(resp);
-	});
+
+	batchCommit(qs, function(result){
+		// changes per second
+		var cps = qs.length / (result.duration_ms / 1000);
+		d.resolve({
+			result: result,
+			changes_per_sec: cps
+		});
+	})
+	
+	// clear query collection again
+	qs = []
+	
 	return d.promise;
 }
 
