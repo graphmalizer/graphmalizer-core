86848547f16422861be92b7d4f303e27d4c551f9
Jelle Herold <jelle@defekt.nl>
2015-07-01 16:04:52 +0200

    use "labels" for types... argh Neo WHY U NO EDGES NORMAL

--- core/graph.js
+++ core/graph.js
@@ -12,11 +12,20 @@ var qs = YAML.parse(fs.readFileSync('./core/graph.yaml', {encoding: 'utf8'}));
 exports.queries = qs.queries
 exports.structures = qs.structures
 
+// lookup query and replace all occurances of «type»
+function substitute(cypher, params)
+{
+	var t = params.type || '_';
+
+	// replace variable «type»
+	return cypher.replace(/«type»/g, t.replace(/:/g, '_'));
+}
+
 // promise makers
 exports.query = function(query_name, params)
 {
-	// lookup query
-	var q = qs.queries[query_name].cypher;
+	// lookup cypher string
+	var q = substitute(qs.queries[query_name].cypher, params);
 
 	// report about it
 	log.QUERY(query_name, q, params);
@@ -24,10 +33,11 @@ exports.query = function(query_name, params)
 	// return promise
 	return cypher({params: params, query: q});
 }
+
 exports.structure = function(structure, action, params)
 {
-	// lookup query
-	var q = qs.structures[structure][action].cypher;
+	// lookup cypher string
+	var q = substitute(qs.structures[structure][action].cypher, params);
 	
 	// report about it
 	log.STRUCTURE(structure, action, q, params);	
