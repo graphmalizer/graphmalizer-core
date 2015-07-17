57779e76aafeef0094e8bf3f3d0e6e80df2f0c07
Jelle Herold <jelle@defekt.nl>
2015-07-17 18:38:58 +0200

    simplified, fixified and logspamified

--- utils/neo4batch.js
+++ utils/neo4batch.js
@@ -1,7 +1,7 @@
-var http = require('http')
-var url = require('url')
+var http = require('http');
 
 var opts = {
+	hostname: 'localhost',
 	port: 7474,
 	method: 'POST',
 	path: '/db/data/transaction/commit',
@@ -9,16 +9,23 @@ var opts = {
 		"Accept": 'application/json; charset=UTF-8',
 		"Content-Type": 'application/json'
 	},
-	auth: 'neo4j:waag',
 	keepAlive: true
 };
 
 var ns_time = function(){
 	var t  = process.hrtime();
 	return t[0] * 1e9 + t[1];
-}
+};
 
-module.exports = function batchCommit(queries, callback){
+/*
+   statements a list of
+   [ { statement: q.query,
+       parameters: q.params }, ... ]
+*/
+
+module.exports = function batchCommit(statements, callback)
+{
+	var s = JSON.stringify({statements: statements});
 
 	// get timestamp and convert to nanoseconds
 	var t0 = ns_time();
@@ -26,41 +33,57 @@ module.exports = function batchCommit(queries, callback){
 	// response
 	var result = '';
 
-	var req = http.request(opts, function(res){
-	   // console.log('STATUS: ' + res.statusCode);
-	   // console.log('HEADERS: ' + JSON.stringify(res.headers));
-	   res.setEncoding('utf8');
+	var req = http.request(opts, function(res) {
+		res.setEncoding('utf8');
 
-	   res.on('data', function (chunk) {
+		res.on('data', function (chunk) {
+			console.log("data");
 			result += chunk;
-	   });
+		});
 
 		res.on('end', function() {
+			console.log("recv end", result.length);
+
 			// parse accumulated response
-			var resp = JSON.parse(result);
+			var resp = {};
 
 			// convert nanoseconds to milliseconds
-			resp.duration_ms = (ns_time() - t0) / 1e6;
+			var t1 = ns_time();
+			resp.duration_ms = (t1 - t0) / 1e6;
+
+			// store
+			resp.statusCode = res.statusCode;
+
+			// result
+			try {
+				resp.result = JSON.parse(result);
+			}
+			catch(e)
+			{
+				console.log("failed parsing result");
+				resp.error = e;
+			}
+
+			console.log("size", resp.result.results.length);
+
+			// parsing duration
+			var t2 = ns_time();
+			resp.parse_ms = (t2 - t1) / 1e6;
 
 			// let caller know
-			callback(resp);
+			callback(null, resp);
 		})
-	})
-
-	req.on('error', function(e) {
-	  console.log('problem with request: ' + e.message);
 	});
 
-	// build request body
-	var body = {statements: []};
-	queries.forEach(function(q){
-		body.statements.push({
-			statement: q.query,
-			parameters: q.params
-		})
+	req.on('error', function(err) {
+		console.log("RECEIVED error EVENT");
+		callback(err, null);
 	});
 
 	// write data to request body
-	req.write(JSON.stringify(body));
+	console.error("writing");
+	req.write(s);
+
+	console.error("send end");
 	req.end();
-}
+};
