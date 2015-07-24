d1081be21e0b11f74b05229ef2c32703f758cb9b
Jelle Herold <jelle@defekt.nl>
2015-07-24 16:23:08 +0200

    examples

---
+++ add_remove.js
@@ -0,0 +1,25 @@
+
+// example
+
+var R = require('ramda');
+var H = require('highland');
+
+var graphmalizer = require('./index');
+
+var ensureAdd = R.assoc('operation', 'add');
+var ensureRemove = R.assoc('operation', 'remove');
+
+// the stream that we will be registering with the graphmalizer
+var addRemoveStream = H();
+
+exports.add = function(thing){
+	addRemoveStream.write(ensureAdd(thing));
+};
+
+exports.remove = function(thing){
+	addRemoveStream.write(ensureRemove(thing));
+};
+
+// register our stream
+graphmalizer(addRemoveStream)
+	.each(H.log);
---
+++ stdin.js
@@ -0,0 +1,33 @@
+
+// another example
+
+// usage: echo '{"type":"pit", "id":"a"}' | node stdin.js
+
+var H = require('highland')
+
+// read NDJSON from standard input
+var stdin = H(process.stdin)
+
+	.splitBy('\n')
+
+	.filter(function(s){
+		return s !== '';
+	})
+
+	.map(function(s){
+		// diff-ish
+		if(/^[+-]/.test(s))
+		{
+			var o = JSON.parse(s.slice(1));
+			o.operation = (s[0] == '+' ? 'add' : 'remove');
+			return o;
+		}
+
+		// regular obj
+		return JSON.parse(s);
+	});
+
+var graphmalizer = require('./index');
+
+graphmalizer(stdin)
+	.each(H.log);
