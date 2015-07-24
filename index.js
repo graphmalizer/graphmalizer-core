1103bea9c14864a50f53d414e20f4489e4546467
Jelle Herold <jelle@defekt.nl>
2015-07-24 16:22:59 +0200

    api api

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-var argv = require('minimist')(process.argv.slice(2));
+var _ = require('highland');
 
-if(argv.redis)
-	require("./core/redis")
-else
-	require("./core/server")
+var graphmalizer = require('./core/stream');
+
+// TODO somehow multiplex / demultiplex streams.. not sure
+// want to return some response
+
+// stream of input streams
+var streams = _();
+
+// processing pipeline
+var system = streams
+	.merge()
+	.pipe(graphmalizer)
+	.each(function(){
+		console.log('~_~_~_~_~ request finished ~_~_~_~_~');
+	});
+
+// api, just give us a input stream
+module.exports = function(stream) {
+	if(!_.isStream(stream))
+		throw new Error("Must pass a (highland) stream");
+
+	streams.write(stream);
+
+	// so that you can look at it
+	return system.observe();
+};
+
