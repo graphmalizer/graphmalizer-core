187af9f217a007e5537b3b4ca6d55519cb2ba830
Jelle Herold <jelle@defekt.nl>
2015-07-31 20:19:47 +0200

    got rid of config loading, inlined API
    - var Graphmalizer = require('graphmalizer');
    - var G = new Graphmalizer({types: ...})
    - G.register( /* stream */ ) => event stream

--- index.js
+++ index.js
@@ -1,128 +1,134 @@
-var _ = require('highland');
 var u = require('util');
-var prttty = require('prttty');
-var Queries = require('./core/queries');
-var argv = require('minimist')(process.argv.slice(2));
+var R = require('ramda');
+var H = require('highland');
 
-var conf = require('./utils/config');
-var log = require('./utils/log');
-var batchCommit = _.wrapCallback(require('./utils/neo4batch'));
+var neoBatch = require('./utils/neo4batch');
+var Queries =  require('./core/queries');
 
-// stream of input streams
-var streams = _();
+var defaultConfig = {
+	Neo4J: {
+		hostname: 'localhost',
+		port: 7474
+	},
 
-// processing pipeline
-var input = streams.merge()
-	.flatMap(prepare)
-	.batchWithTimeOrCount(argv.batchTimeout || 1000, argv.batchSize || 2500);
+	batchTimeout: 1000,
+	batchSize: 2500,
 
-var output = input.fork()
-	.map(batchCommit) // a -> stream b
-	.series()
-	.map(unwrapResult);
-
-// zip into [request, response]
-var rr = input.fork()
-	.zip(output);
-
-var I = rr.fork().pluck(0).sequence();
-var O = rr.fork().pluck(1).sequence();
-var system = I.zip(O)
-	.map(function(rr){
-		return {
-			request: rr[0],
-			response: rr[1]
+	// behave like a graph with equivalences
+	types: {
+		node: {
+			node: {}
+		},
+		arc: {
+			arc: {}
+		},
+		equivalence: {
+			equivalence: {}
 		}
-	});
+	}
+};
 
-module.exports = function(stream) {
+function Graphmalizer(config)
+{
+	// store configuration with user overrides
+	var conf = R.merge(defaultConfig, config || {});
 
-	if(stream)
+	// setup neo4j client
+	var batchCommit = H.wrapCallback(neoBatch(conf.Neo4J));
+
+	// make query (uses config to determine type ~ structure mapping)
+	var types = Object.keys(conf.types);
+
+	function prepare(o)
 	{
-		if(!_.isStream(stream))
-			throw new Error("Must pass a (highland) stream");
+		// default operation is add
+		o.operation = o.operation || 'add';
 
-		streams.write(stream);
+		// default dataset is stdin
+		o.dataset = o.dataset || 'stdin';
+
+		// s, t alias for source/target
+		o.source = o.source || o.s;
+		o.target = o.target || o.t;
+
+		// ensure (empty) data field
+		o.data = o.data || {};
+
+		// check if we have defined the type
+		if(!conf.types[o.type])
+		{
+			console.error(u.format('Unknown type "%s", must be one of: %s', o.type, types));
+			return []
+		}
+
+		// lookup structure (based on type)
+		o.structure = Object.keys(conf.types[o.type])[0];
+
+		try
+		{
+			return [Queries.mkQuery(o)];
+		}
+		catch(err)
+		{
+			console.error(err.stack);
+			return []
+		}
 	}
 
-	// so that you can operate on it
-	return system.fork();
+	// stream of input streams
+	this.inputs = H();
+
+	// merge all inputs, convert to cypher queries and batch up
+	var kk = this.inputs
+		.merge()
+		.flatMap(prepare);
+
+	kk.observe(H.log);
+
+	var input =  kk.batchWithTimeOrCount(conf.batchTimeout, conf.batchSize);
+
+	// commit batches sequentially
+	var output = input
+		.fork()
+		.map(batchCommit) // a -> stream b
+		.series()
+		.pluck('results');
+
+	// zip into [request-batch, response-batch]
+	var rr = input
+		.fork()
+		.zip(output);
+
+	// unzip, flatten batches
+	var requests = rr.fork().pluck(0).sequence();
+	var responses = rr.fork().pluck(1).sequence();
+
+	// zip back up and turn it into a dictionary
+	this.system = requests
+		.zip(responses)
+		.map(function(rr){
+			return {
+				request: rr[0],
+				response: rr[1]
+			}
+		});
+}
+
+// subscribe a stream to the graphmalizer
+Graphmalizer.prototype.register = function(stream)
+{	// ensure valid arguments
+	if(stream)
+	{
+		if(!H.isStream(stream))
+			throw new Error("Must pass a (highland) stream");
+
+		// register input stream
+		this.inputs.write(stream);
+	}
+
+	// return stream of all request-responses
+	return this.system.fork();
 };
 
 
-// make query
-function prepare(o)
-{
-	// default operation is add
-	o.operation = o.operation || 'add';
-
-	// default dataset is stdin
-	o.dataset = o.dataset || 'stdin';
-
-	// s, t alias for source/target
-	o.source = o.source || o.s;
-	o.target = o.target || o.t;
-
-	// ensure (empty) data field
-	o.data = o.data || {};
-
-	// check if we have defined the type
-	if(! conf.types[o.type])
-	{
-		console.error(u.format('Unknown type "%s", must be one of: %s', o.type, Object.keys(conf.types)));
-		return []
-	}
-
-	// lookup structure (based on type)
-	o.structure = Object.keys(conf.types[o.type])[0];
-
-	//console.log(prttty.render(o));
-
-	try
-	{
-		return [Queries.mkQuery(o)];
-	}
-	catch(err)
-	{
-		console.error(err.stack);
-		return []
-	}
-}
-
-/*
-	results: [
-		{ columns: ["n"]
-		, data: [
-			{ row: [
-				{ id: "a"
-				, dataset: "stdin"
-				, accessTime: 1437955066039
-				, counter: 1
-				}
-			]
-		}]
-	}]
-
-	~>
-
-	[{n:
-		{ id: "a"
-		, dataset: "stdin"
-		, accessTime: 1437955066039
-		, counter: 1
-		}
-	}]
-*/
-
-function unwrapResult(x){
-	// create dictionary for each row
-	// {columnName: row[ columnIndex ]}
-	return x.result.results.map(function(entry){
-		return entry.data.map(function(d){
-			return entry.columns.reduce(function(acc, n, i){
-				acc[n] = d.row[i];
-				return acc;
-			}, {});
-		});
-	});
-}
+module.exports = Graphmalizer;
