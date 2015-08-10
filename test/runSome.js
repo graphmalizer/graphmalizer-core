f18af27c45dbc811b5c7354cefc547f604dba14c
Jelle Herold <jelle@defekt.nl>
2015-08-10 10:01:20 +0200

    run permutations of tests

--- test/runSome.js
+++ test/runSome.js
@@ -1,48 +1,80 @@
-// another example
+// runs some tests
 var Graphmalizer = require('../index');
 var H = require('highland');
 var R = require('ramda');
 var pp = require('prttty');
+var u = require('util');
 
-// tester
+// test anything protocol
 var test = require('tape').test;
 
 // load tests from file
 var fs = require('fs');
 var yaml = require('js-yaml');
-var f = fs.readFileSync('test/runSome.yaml', {encoding: 'utf8'});
-var spec = yaml.safeLoad(f, {filename: './tests.yaml'});
+var fn = require('path').join(__dirname, 'runSome.yaml');
+var f = fs.readFileSync(fn, {encoding: 'utf8'});
+var spec = yaml.safeLoad(f, {filename: fn});
 
+// permutation stream
+var permutator = require('../utils/permutations');
 
-function run(tname, statements) {
-
-	// make test stream
-	var s = R.prepend({query:'clear'}, statements);
-	var tests = H(s)
-		.map(function(statement){
-			statement.dataset = 'test';
-			return statement;
-		});
-
+function runOne(tname, statements)
+{
+	// make and run test
 	test(tname, function(t){
 
-		t.plan(s.length);
+		t.plan(statements.length);
+
+		var rs = [];
 
 		// instantiate
 		var G = new Graphmalizer({batchTimeout: 10});
-
-		var g = G
-			.register(tests)
+		G.register(H(statements))
 			.each(function(x){
 				t.assert(x.response.length == 1, "we have one row in response");
+				rs.push(x);
 			})
 			.done(function(){
 				console.log('done');
+
+				// compare result
+				var graph = R.last(rs);
+				console.log(graph);
+
 				t.end();
-			})
+			});
 	})
 }
 
-for(tname in spec.alt)
-	run(tname, spec.alt[tname]);
+var wrap = R.compose(
+	// clean DB before
+	R.prepend({query:'clear'}),
+
+	// and query it after
+	R.append({query:'graph'}),
+
+	// ensure we have a dataset field
+	R.map(R.assoc('dataset', 'test'))
+);
+
+function runPermutations(tname, statements)
+{
+	console.log("b", tname, statements);
+	var iteration = 0;
+	H(statements)
+		.pipe(permutator())
+		.fork()
+		.each(function(xs){
+			// clear -> [statements] -> query
+			var ss = wrap(xs);
+
+			// generate a test name for this iteration
+			var n = u.format('%s-%d', tname, iteration++);
+
+			runOne(n, ss)
+		});
+}
+
+for(tname in spec)
+	runPermutations(tname, spec[tname].operations);
 
