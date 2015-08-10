98a27cb18e1af924d1c199c6b2b908fcddb6fbd9
Jelle Herold <jelle@defekt.nl>
2015-08-10 13:11:09 +0200

    pass --debug to spew logs

--- utils/log.js
+++ utils/log.js
@@ -1,3 +1,3 @@
 // this should do until we get real log system
 var argv = require('minimist')(process.argv.slice(2));
-module.exports = argv.stfu ? function(){} : console.log.bind(console);
+module.exports = argv.debug ? console.log.bind(console) : function(){};
